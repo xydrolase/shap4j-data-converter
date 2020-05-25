f07d55e60ddab07546f783d9c57d97fa591e0f93
Xin Yin <killkeeper@gmail.com>
2020-05-25 15:02:39 -0400

    First pass of the data converter.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+def main():
+    setup(
+        name="shap4j-data-converter",
+        version="0.0.1",
+        description="Data converter for shap4j",
+        install_requires=['shap', 'numpy'],
+        scripts=['bin/shapjsconv'],
+
+        author="Xin Yin",
+        author_email="xydrolase@gmail.com"
+    )
+
+
+if __name__ == "__main__":
+    main()
